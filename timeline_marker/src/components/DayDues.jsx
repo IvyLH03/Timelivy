928b379a911767f224ef7cf43908e6989a740dc3
IvyLH03 <hanzhangzhu@outlook.com>
2024-08-22 21:17:16 -0700

    clear branch

--- timeline_marker/src/components/DayDues.jsx
+++
@@ -1,7 +0,0 @@
-export default function DayDues(props) {
-    return (
-        <div className="timeline-due-dates">
-            <p></p>
-        </div>
-    )
-}
--- timeline_marker/src/components/DayHeader.jsx
+++
@@ -1,8 +0,0 @@
-
-export default function DayHeader(props) {
-    return (
-    <div className="timeline-date">
-        <p>{props.date}</p>
-        <p>{props.day}</p>
-    </div>)
-}
