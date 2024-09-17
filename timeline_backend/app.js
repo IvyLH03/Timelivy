4a27cdcaa8881fc578f3bba87abf741e467f4259
Ivy Zhu <eurekazhu@gmail.com>
2024-09-16 20:34:52 -0500

    sqlite

--- timeline_backend/app.js
+++ timeline_backend/app.js
@@ -12,26 +12,47 @@ applyLooseCORSPolicy(app);
 applyBodyParsing(app);
 applyLogging(app);
 
+const INSERT_DATA_SQL = 'INSERT INTO TestData(value) VALUES (?) RETURNING id;'
+const GET_DATA_SQL = 'SELECT * FROM TestData'
+
 const db = await open({
   filename: "./db.db",
   driver: sqlite3.Database
 });
 
-let tmp_mem = []
+await db.exec("CREATE TABLE IF NOT EXISTS TestData(id INTEGER PRIMARY KEY UNIQUE, value TEXT NOT NULL);")
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.get('/test/:uid', (req, res) => {
+app.get('/test', (req, res) => {
   res.status(200).send({msg:tmp_mem})
 })
 
-app.post('/test_put', (req, res) => {
-  tmp_mem = [...tmp_mem, req.body.data]
-  res.status(200).send({msg:"done"})
-  
+app.post('/test_put', async (req, res) => {
+  try {
+    const ret = await db.get(INSERT_DATA_SQL, req.body.data)
+    console.log(ret)
+    res.status(200).send(ret)
+  } catch (e) {
+    res.status(500).send({msg:"something went wrong"})
+    console.log(e)
+  }
 })
+
+app.get('/test_get', async (req, res) => {
+  try {
+    const ret = await db.all(GET_DATA_SQL)
+    console.log(ret)
+    res.status(200).send(ret)
+  } catch (e) {
+    res.status(500).send({msg:"something went wrong"})
+    console.log(e)
+  }
+})
+
 
 applyErrorCatching(app)
 
